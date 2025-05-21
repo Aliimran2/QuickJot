35ab3226acd7061da903f6b6d670d17f05336fd0
MIAS <muhammadimran.math@gmail.com>
2025-05-21 17:21:58 +0500

    Hilt in Application is added

--- app/src/main/java/com/miassolutions/quickjot/application/QuickJotApp.kt
+++ app/src/main/java/com/miassolutions/quickjot/application/QuickJotApp.kt
@@ -1,6 +1,8 @@
 package com.miassolutions.quickjot.application
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class QuickJotApp : Application() {
 }
